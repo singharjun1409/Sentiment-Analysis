6d8685ce77e6b453f365d9b06312b3ae96b24e5c
Arjun Singh <as16405@nyu.edu>
2025-04-14 21:57:45 -0400

    Complete

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,14 +1,33 @@
-import nltk
-import ssl
 from nltk.sentiment import SentimentIntensityAnalyzer
-# Download VADER lexicon
-#ssl._create_default_https_context = ssl._create_unverified_context
-#nltk.download('vader_lexicon')
-# Initialize VADER sentiment analyzer
-sid = SentimentIntensityAnalyzer()
-# Sample text for sentiment analysis
-text = "I love this product! It's amazing."
-# Perform sentiment analysis
-sentiment_score = sid.polarity_scores(text)
-# Print sentiment score 
-print(sentiment_score)
+import re
+from collections import Counter
+
+def get_reviews(filepath):
+    with open(filepath , "r") as f:
+        text = f.read()
+        return(re.findall(r'"(.*?)"' , text))
+
+def analyze_sentiments(filepath):
+    reviews = get_reviews(filepath)
+
+    # Initialize VADER sentiment analyzer
+    sid = SentimentIntensityAnalyzer()
+    analysis = []
+
+    for review in reviews:
+        # Perform sentiment analysis
+        sentiment_score = sid.polarity_scores(review)
+        analysis.append(sentiment_score)
+    
+    tally = Counter()
+    for score in analysis:
+        if score["compound"] == 0:
+            tally["Neutral"] += 1
+        elif score["compound"] > 0:
+            tally["Positive"] += 1
+        else:
+            tally["Negative"] += 1
+
+    for key in tally:
+        print(f"Review: {key} , Count: {tally[key]}")
+analyze_sentiments("reviews.txt")
